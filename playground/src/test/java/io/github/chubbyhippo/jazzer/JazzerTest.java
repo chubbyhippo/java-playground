7ae0189eed6d5f8fe7ccd1eca6e3fef79aa18888
chubbyhippo <mkmsacc@outlook.com>
2025-12-03 13:50:11 +0700

    Add null assertion in `fuzzMe` method of `JazzerTest` using AssertJ

--- playground/src/test/java/io/github/chubbyhippo/jazzer/JazzerTest.java
+++ playground/src/test/java/io/github/chubbyhippo/jazzer/JazzerTest.java
@@ -4,11 +4,14 @@ import com.code_intelligence.jazzer.junit.FuzzTest;
 import com.code_intelligence.jazzer.mutation.annotation.NotNull;
 import com.code_intelligence.jazzer.mutation.annotation.WithUtf8Length;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class JazzerTest {
 
     @FuzzTest
     void fuzzMe(@NotNull @WithUtf8Length(min = 10, max = 100) String input) {
         System.out.println(input);
+        assertThat(input).isNotNull();
     }
 
 }
