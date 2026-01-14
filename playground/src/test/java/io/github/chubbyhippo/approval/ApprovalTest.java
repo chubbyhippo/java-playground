f50167f72c1aa5b2ad9e722d4e58dd757b878ba0
chubbyhippo <mkmsacc@outlook.com>
2026-01-14 21:59:28 +0700

    Add `ApprovalTest` class for combination-based approval testing

---
+++ playground/src/test/java/io/github/chubbyhippo/approval/ApprovalTest.java
@@ -0,0 +1,25 @@
+package io.github.chubbyhippo.approval;
+
+import org.approvaltests.combinations.CombinationApprovals;
+import org.junit.jupiter.api.Test;
+
+public class ApprovalTest {
+
+    @Test
+    void testCombinations() {
+        String[] inputs1 = {"A", "B"};
+        Integer[] inputs2 = {1, 2, 3};
+        Boolean[] inputs3 = {true, false};
+
+        CombinationApprovals.verifyAllCombinations(
+                this::doSomething,
+                inputs1,
+                inputs2,
+                inputs3
+        );
+    }
+
+    private String doSomething(String s, Integer i, Boolean b) {
+        return String.format("Result: %s, %d, %b", s, i, b);
+    }
+}
