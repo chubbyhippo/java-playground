308cbffc9657940bfca8489f8e5c4c422fd491b7
chubbyhippo <mkmsacc@outlook.com>
2026-01-14 21:54:26 +0700

    Apply consistent indentation in `JazzerTest` for code style compliance

--- playground/src/test/java/io/github/chubbyhippo/jazzer/JazzerTest.java
+++ playground/src/test/java/io/github/chubbyhippo/jazzer/JazzerTest.java
@@ -1,17 +1,16 @@
 package io.github.chubbyhippo.jazzer;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.code_intelligence.jazzer.junit.FuzzTest;
 import com.code_intelligence.jazzer.mutation.annotation.NotNull;
 import com.code_intelligence.jazzer.mutation.annotation.WithUtf8Length;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class JazzerTest {
 
-    @FuzzTest
-    void fuzzMe(@NotNull @WithUtf8Length(min = 10, max = 100) String input) {
-        System.out.println(input);
-        assertThat(input).isNotNull();
-    }
-
+  @FuzzTest
+  void fuzzMe(@NotNull @WithUtf8Length(min = 10, max = 100) String input) {
+    System.out.println(input);
+    assertThat(input).isNotNull();
+  }
 }
