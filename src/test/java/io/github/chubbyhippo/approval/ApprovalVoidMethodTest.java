4395fcc218a0324e6bd370207c86748399ef6871
chubbyhippo <mkmsacc@outlook.com>
2026-01-18 20:40:08 +0700

    Refactor `ApprovalVoidMethodTest` to fix formatting inconsistencies and clean up imports

--- src/test/java/io/github/chubbyhippo/approval/ApprovalVoidMethodTest.java
+++ src/test/java/io/github/chubbyhippo/approval/ApprovalVoidMethodTest.java
@@ -1,28 +1,27 @@
 package io.github.chubbyhippo.approval;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import org.approvaltests.Approvals;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
 public class ApprovalVoidMethodTest {
 
-    @Test
-    void testVoidMethodOutput() {
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        PrintStream originalOut = System.out;
-        System.setOut(new PrintStream(outputStream));
+  @Test
+  void testVoidMethodOutput() {
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    PrintStream originalOut = System.out;
+    System.setOut(new PrintStream(outputStream));
 
-        try {
-            printHello();
-            Approvals.verify(outputStream.toString());
-        } finally {
-            System.setOut(originalOut);
-        }
+    try {
+      printHello();
+      Approvals.verify(outputStream.toString());
+    } finally {
+      System.setOut(originalOut);
     }
+  }
 
-    private void printHello() {
-        System.out.println("Hello, Approval Tests!");
-    }
+  private void printHello() {
+    System.out.println("Hello, Approval Tests!");
+  }
 }
