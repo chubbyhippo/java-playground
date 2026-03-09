5c72daabcae525a22dab2cb6131063cde75f769d
chubbyhippo <mkmsacc@outlook.com>
2026-03-09 13:55:35 +0700

    Refactor `ApprovalVoidMethodTest` to use `var` and fix inconsistent indentation

--- src/test/java/io/github/chubbyhippo/approval/ApprovalVoidMethodTest.java
+++ src/test/java/io/github/chubbyhippo/approval/ApprovalVoidMethodTest.java
@@ -2,26 +2,27 @@ package io.github.chubbyhippo.approval;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+
 import org.approvaltests.Approvals;
 import org.junit.jupiter.api.Test;
 
 public class ApprovalVoidMethodTest {
 
-  @Test
-  void testVoidMethodOutput() {
-    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-    PrintStream originalOut = System.out;
-    System.setOut(new PrintStream(outputStream));
+    @Test
+    void testVoidMethodOutput() {
+        var outputStream = new ByteArrayOutputStream();
+        var originalOut = System.out;
+        System.setOut(new PrintStream(outputStream));
 
-    try {
-      printHello();
-      Approvals.verify(outputStream.toString());
-    } finally {
-      System.setOut(originalOut);
+        try {
+            printHello();
+            Approvals.verify(outputStream.toString());
+        } finally {
+            System.setOut(originalOut);
+        }
     }
-  }
 
-  private void printHello() {
-    System.out.println("Hello, Approval Tests!");
-  }
+    private void printHello() {
+        System.out.println("Hello, Approval Tests!");
+    }
 }
